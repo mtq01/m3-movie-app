b810debfd5f341845f9b6f5dec31651cc769ab98
Mahtab Salimzadeh <mahtab@Mahtabs-MacBook-Air.local>
2026-01-27 18:32:49 -0800

    normalize.css added into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from 'react';
 import './styles/index.css';
+import './styles/normalize.css';
 import Nav from './components/Nav/Nav';
 import Footer from './components/Footer/Footer';
 
