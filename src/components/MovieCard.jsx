834980ca092c0583c79db24908b0a53c9c4c78dc
mv-nguyen <hnguyen324@my.bcit.ca>
2026-01-31 21:59:41 -0800

    Added styling to for the movie card layout. Reorganized the movie card html and added wrapper span to the movie content.

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -12,16 +12,16 @@ function MovieCard({
       <div className="movie-poster">
         {/* Poster image (placeholder for now) */}
         <img src={poster} alt={title} />
-
-        {/* Favourite button is visual only for now
-            Logic/state will be added in a later step */}
-        <button className="favourite" aria-label="Add to favourites">
-          ☆
-        </button>
       </div>
 
       <div className="movie-content">
-        <h3 className="movie-title">{title}</h3>
+        <span className="movie-card-row-1">
+          <h3 className="movie-title">{title}</h3>
+          {/* Favourite button is visual only for now */}
+          <button className="favourite" aria-label="Add to favourites">
+            FAV
+          </button>
+        </span>
         <p className="movie-date">{release_date}</p>
         <p className="movie-overview">{overview}</p>
 
