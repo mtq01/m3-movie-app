1388b157de7c1b71917b31d8ff4e3005fb8d9b5c
mv-nguyen <hnguyen324@my.bcit.ca>
2026-01-31 20:39:45 -0800

    Added in files to test data importing for the movie cards

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,37 @@
+function MovieCard({
+  poster,
+  title,
+  release_date,
+  overview,
+  details_link,
+  is_favourite, // not used yet, but we'll need this later for toggling favourites
+}) {
+  return (
+    <article className="movie-card">
+      <div className="movie-poster">
+        {/* Poster image (placeholder for now) */}
+        <img src={poster} alt={title} />
+
+        {/* Favourite button is visual only for now
+            Logic/state will be added in a later step */}
+        <button className="favourite" aria-label="Add to favourites">
+          ☆
+        </button>
+      </div>
+
+      <div className="movie-content">
+        <h3 className="movie-title">{title}</h3>
+        <p className="movie-date">{release_date}</p>
+        <p className="movie-overview">{overview}</p>
+
+        {/* Learn more link
+            This will later become a router link */}
+        <a href={details_link} className="learn-more">
+          Learn more
+        </a>
+      </div>
+    </article>
+  );
+}
+
+export default MovieCard;
---
+++ src/data/movieList.js
@@ -0,0 +1,60 @@
+/* MICHELLE NOTE - This file is only for testing and will be replaced or deleted later */
+
+const movieList = [
+  {
+    id: 1,
+    title: "Movie Title One",
+    poster: "POSTER_PLACEHOLDER",
+    release_date: "2026-01-01",
+    overview: "placeholder overview text",
+    details_link: "/movies/1",
+    is_favourite: false,
+  },
+  {
+    id: 2,
+    title: "Movie Title Two",
+    poster: "POSTER_PLACEHOLDER",
+    release_date: "2026-01-01",
+    overview: "placeholder overview text",
+    details_link: "/movies/2",
+    is_favourite: false,
+  },
+  {
+    id: 3,
+    title: "Movie Title Three",
+    poster: "POSTER_PLACEHOLDER",
+    release_date: "2026-01-01",
+    overview: "placeholder overview text",
+    details_link: "/movies/3",
+    is_favourite: false,
+  },
+  {
+    id: 4,
+    title: "Movie Title Four",
+    poster: "POSTER_PLACEHOLDER",
+    release_date: "2026-01-01",
+    overview: "placeholder overview text",
+    details_link: "/movies/4",
+    is_favourite: false,
+  },
+  {
+    id: 5,
+    title: "Movie Title Five",
+    poster: "POSTER_PLACEHOLDER",
+    release_date: "2026-01-01",
+    overview: "placeholder overview text",
+    details_link: "/movies/5",
+    is_favourite: false,
+  },
+  {
+    id: 6,
+    title: "Movie Title Six",
+    poster: "POSTER_PLACEHOLDER",
+    release_date: "2026-01-01",
+    overview: "placeholder overview text",
+    details_link: "/movies/6",
+    is_favourite: false,
+  },
+];
+
+export default movieList;
