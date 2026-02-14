f938869a8460b9700ffa0ff6699c678afe0cfac5
mv-nguyen <hnguyen324@my.bcit.ca>
2026-02-13 23:57:52 -0800

    Updated inline comments for spelling mistakes and clearer comments

--- src/globals/globals.js
+++ src/globals/globals.js
@@ -3,15 +3,15 @@ export const appTitle = "Cinemax";
 
 /* API Variables + Key */
 
-// variables for the different movie genres
+// variables for the different movie categories
 export const endPointPopular = "https://api.themoviedb.org/3/movie/popular";
 export const endPointTopRated = "https://api.themoviedb.org/3/movie/top_rated";
 export const endPointNowPlaying =
   "https://api.themoviedb.org/3/movie/now_playing";
 export const endPointUpcoming = "https://api.themoviedb.org/3/movie/upcoming";
 
-// Imported API Key from TMDB -> Stored in root ENV to protect from public
+// API Key imported from .env
 export const apiKey = import.meta.env.VITE_MOVIE_API;
 
-/* Example of concacenating API with variables for API Call below
+/* Example of API Call with the set variables
 >>> const url = `${endPointNowPlaying}?api_key=${apiKey}`; <<< */
