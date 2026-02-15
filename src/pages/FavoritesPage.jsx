243fa10385076a6499e9c36394809c86df313ab4
mv-nguyen <hnguyen324@my.bcit.ca>
2026-02-14 17:35:33 -0800

    Added FavoritesPage.css and added in no favorites html content

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,6 +1,7 @@
 // Page - Favorites
 import { useEffect } from "react";
 import { appTitle } from "../globals/globals";
+import "../styles/FavoritesPage.css";
 
 // dynamic page title
 const Favorites = () => {
@@ -10,11 +11,25 @@ const Favorites = () => {
 
   return (
     <section>
-      {/* Page Specific Components*/}
+      <div className="heading-container">
+        <svg
+          className="fav-icon"
+          xmlns="http://www.w3.org/2000/svg"
+          height="24px"
+          viewBox="0 -960 960 960"
+          width="24px"
+        >
+          <path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Z" />
+        </svg>
 
-      {/* Placeholders - <h2> <p> and <section> can be deleted */}
-      <h2>My List - Favorites</h2>
-      <p>List of favorites goes here</p>
+        <h2>My List</h2>
+      </div>
+      <div className="fav-content-container">
+        <p>
+          You have no favorite movies, return to the <a>homepage</a> to add a
+          favourite movie
+        </p>
+      </div>
     </section>
   );
 };
