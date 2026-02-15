aafdd1d914ec82e937d88032f664c1168d4d1e51
MIKE <miket01927@gmail.com>
2026-02-15 00:09:45 -0800

    Moved the getTrailer logic from Carousel.jsx into trailerPopupUtil.js. This enables the component to be reused everywhere via imports rather than everyone typing their own logic. It talks to the TMDB API, searches through the movie data for trailers, teasers, etc. It picks out specific YT keys for a trailer and handles errors if the internet goes down or the api fails. Carousel.jsx was trying to do everything in one place, it included carousel logic, a timer, data-fetching, and a popup manager all at once. Moving it to its own file is better for scalability

---
+++ src/utility/trailerPopupUtil.js
@@ -0,0 +1,37 @@
+import { apiKey, endPointTrailer } from "../globals/globals.js";
+
+/* 
++++++ getTrailer function. +++++
+- [input] movieId is the unque id for the movie from TMDB
+- [output] returns the UT 'key' string if found, otherwise returns null
+*/
+
+export const getTrailer = async (movieId) => {
+    // grab the specific api URL for movies
+    const movieUrl = `${endPointTrailer}${movieId}/videos?api_key=${apiKey}`;
+
+    try {
+        const response = await fetch(movieUrl);
+        const data = await response.json();
+
+        // returns an array of videos
+        // .find() grabs the 1st one that is a 'trailer' thats hosted on YT
+        const video = data.results.find(
+            (vid) => vid.type === "Trailer" && vid.site === "YouTube"
+        );
+
+        // if matching video found, return the key. otherwise return null.
+        if (video) {
+            return video.key;
+        } else {
+            // returns null so app dont crash if data not found
+            return null;
+        }
+
+    } catch (error) {
+        // log debugging error, returns null if fetch fails (prevents trying to render a broken video)
+        // broken video = a file that wont play
+        console.error("Error fetching trailer:", error);
+        return null;
+    }
+};
