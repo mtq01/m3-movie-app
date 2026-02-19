d3aa59af6858b384fff4d83091aa64a578537d62
mv-nguyen <hnguyen324@my.bcit.ca>
2026-02-18 22:09:39 -0800

    Added comment to the wrong file, should be in Apps.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./styles/index.css";
 import "./styles/normalize.css";
 import AppRouter from "./routers/AppRouter";
 
+//Provider wraps entire application so the redux store is available to every component
 function App() {
   return (
     <>
