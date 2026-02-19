a207ec210f2b01b5142265047178b4ab5aa074ae
mv-nguyen <hnguyen324@my.bcit.ca>
2026-02-18 20:04:10 -0800

    Fixed bug in the removeFav action logic by removing the index function and using movie.id comparision instead. Movies are now being stored in the redux properly

--- src/features/favs/favsSlice.jsx
+++ src/features/favs/favsSlice.jsx
@@ -24,11 +24,6 @@ const initialState = {
   favMovies: favsFromLocalStorage.favMovies,
 };
 
-// This helper function finds the index of movieToRemove inside of the favMovies array by loop through the movie ids until it finds a match.
-function getMovieIndex(movieToRemove, favMovies) {
-  return favMovies.findIndex((movie) => movie.id === movieToRemove.id);
-}
-
 export const favsSlice = createSlice({
   name: "favs",
   initialState,
@@ -44,18 +39,16 @@ export const favsSlice = createSlice({
       state.favMovies = newFavs;
     },
 
-    // This reducer runs when user clicks on the fav button again to remove it from favorites
-    // Movie gets removed from favMovies and updates the local storage
+    // This reducer runs when the user clicks the fav button again to remove a movie
     removeFav: (state, action) => {
-      // Makes copy of the current favorites array
-      const favsCopy = [...state.favMovies];
-      //Remove the clicked movie from index
-      //Splice(index, 1) removes ONE movie from index
-      favsCopy.splice(getMovieIndex(action.payload, state.favMovies), 1);
+      // filter() used  to create a new array from favMovies and keeps the movie ids that does not match the clicked movie
+      const updatedFavs = state.favMovies.filter((movie) => {
+        return movie.id !== action.payload.id;
+      });
       // Save updated favorites into local storage
-      localStorage.setItem(appStorageName, JSON.stringify(favsCopy));
-      //Updates redux store
-      state.favMovies = favsCopy;
+      localStorage.setItem(appStorageName, JSON.stringify(updatedFavs));
+      //Updates redux store with the new array
+      state.favMovies = updatedFavs;
     },
   },
 });
--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,4 +1,3 @@
-// Page - Favorites
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import MovieCard from "../components/MovieCard";
@@ -6,13 +5,13 @@ import { NavLink } from "react-router-dom";
 import { appTitle } from "../globals/globals";
 import "../styles/FavoritesPage.css";
 
-// dynamic page title
 const Favorites = () => {
   useEffect(() => {
     document.title = `Favorites | ${appTitle}`;
   }, []);
 
   const favs = useSelector((state) => state.favs.favMovies);
+  console.log(favs);
 
   return (
     <section className="favorites">
@@ -25,33 +24,33 @@ const Favorites = () => {
           >
             <path d="M200-120v-640q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v640L480-240 200-120Z" />
           </svg>
-
           <h2>My List</h2>
         </div>
+
         <div className="fav-content-container">
           <div className="no-movie-msg-wrapper">
-       
-            {favs.length < 1 ?      <h3>Sorry!</h3>
-              <p>
-                You have no favorite movies, return to the{" "}
-                <NavLink to="/">homepage</NavLink> to add a favourite movie.
-              </p>
-            :
+            {favs.length < 1 ? (
+              <>
+                <h3>Sorry!</h3>
+                <p>
+                  You have no favorite movies. Return to the{" "}
+                  <NavLink to="/">homepage</NavLink> to add a favourite movie.
+                </p>
+              </>
+            ) : (
               <div className="fav-movie-grid">
-                {favs.map((singleMovie, i) => {
-                  return (
-                    <MovieCard
-                      key={singleMovie.id}
-                      id={singleMovie.id}
-                      title={singleMovie.title}
-                      poster={singleMovie.poster}
-                      release_date={singleMovie.release_date}
-                      overview={singleMovie.overview}
-                      details_link={`/details?id=${singleMovie.id}`}
-                      isFav={true}
-                    />
-                  );
-                })}
+                {favs.map((singleMovie) => (
+                  <MovieCard
+                    key={singleMovie.id}
+                    id={singleMovie.id}
+                    title={singleMovie.title}
+                    poster={singleMovie.poster}
+                    release_date={singleMovie.release_date}
+                    overview={singleMovie.overview}
+                    details_link={`/details?id=${singleMovie.id}`}
+                    isFav={true}
+                  />
+                ))}
               </div>
             )}
           </div>
